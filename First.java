c9177a6a3dc6bd298d2b9d1e0f28a0ae5bf21b35
Rashmi <rashmiwaghmare1@gmail.com>
2023-12-01 14:44:45 +0530

    One First java file is added

---
+++ First.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class First {
+public static void main(String[] args) {
+    ArrayList<Integer> ls= new ArrayList<>();
+    ls.add(10);
+    ls.add(50);
+    ls.add(60);
+    System.out.println(ls);
+    System.out.println(ls.size());
+}
+}
