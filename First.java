232b2fe22f38d99db0646398f624010d3a63da5b
Rashmi <rashmiwaghmare1@gmail.com>
2023-12-02 09:53:45 +0530

    modified

--- First.java
+++ First.java
@@ -2,11 +2,13 @@ import java.util.ArrayList;
 
 public class First {
 public static void main(String[] args) {
-    ArrayList<Integer> ls= new ArrayList<>();
-    ls.add(10);
-    ls.add(50);
-    ls.add(60);
-    System.out.println(ls);
-    System.out.println(ls.size());
+   ArrayList<String> ls=new ArrayList<>();
+ls.add("Rashmi");
+ls.add("Riddhi");
+ls.add("Waghmare");
+
+System.out.print(ls);
+
+
 }
 }
