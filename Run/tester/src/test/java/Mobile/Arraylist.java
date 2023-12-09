207c195d7849a300446d81639c5c73b9b01b6353
Rashmi <rashmiwaghmare1@gmail.com>
2023-12-09 17:43:44 +0530

    one Arraylist class is created

---
+++ Run/tester/src/test/java/Mobile/Arraylist.java
@@ -0,0 +1,15 @@
+package Mobile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> ls=new ArrayList<>();
+        ls.add(10);
+        ls.add(90);
+        ls.add(40);
+        System.out.println(ls);
+        System.out.println(ls.size());
+    }
+}
